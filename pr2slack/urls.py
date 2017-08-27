6034dc9d8c75f0e4bd8eddb8928662121cdfc9a5
cgetc <cgetc502@gmail.com>
2017-08-27 23:59:45 +0900

    staticfilesのurlpatternsを追加

--- pr2slack/urls.py
+++ pr2slack/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
-from django.contrib import admin, auth
-
+from django.contrib import admin
+from django.views import static
 from . import views
 
 urlpatterns = [
+    url(r'^static/(?P<path>.*)$', static.serve, kwargs={'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', admin.site.urls),
     url('', include('social_django.urls', namespace='social')),
     url(r'^logout/$', views.logout, name='logout'),
