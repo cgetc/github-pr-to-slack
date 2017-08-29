30adce91c52cfaabc9c757ab8721772d9e4a545e
cgetc <cgetc502@gmail.com>
2017-08-29 18:04:32 +0900

    PullReqeustThreadを管理画面に追加

--- pr2slack/admin.py
+++ pr2slack/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from pr2slack.models import GithubSecret
+from pr2slack.models import GithubSecret, PullReqeustThread
 
-
+admin.site.register(PullReqeustThread)
 admin.site.register(GithubSecret)
